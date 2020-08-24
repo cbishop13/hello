bb1417c4398bd5fa85eee7196d120518821ff76b
cbishop13fgit config --global user.name cbishop13 <cbishop13@husker.unl.edu>
2020-08-24 23:04:13 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Collin Bishop
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **arvg) {
+
+ printf("Hello World!\n");
+
+     return 0;
+ }
